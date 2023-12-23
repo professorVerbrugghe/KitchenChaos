4f75bd9f59d645122ddc1e569a207a12369e6246
professorVerbrugghe <lotrdetailsys@gmail.com>
2023-12-23 12:52:18 +0100

    Refactored code - input vector to secondary script

---
+++ UprjKitchenChaos/Assets/Scripts/GameInput.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInput : MonoBehaviour {
+
+
+    public Vector2 GetMovementVectorNormalised() {
+        //initialize input vector
+        Vector2 inputVector = new Vector2(0, 0);
+
+        //calculate input vector with input
+        if (Input.GetKey(KeyCode.W)) {
+            inputVector.y = +1;
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            inputVector.y = -1;
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            inputVector.x = -1;
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            inputVector.x = 1;
+        }
+
+        //normalize vector
+        inputVector = inputVector.normalized;
+        return inputVector;
+    }
+}
--- UprjKitchenChaos/Assets/Scripts/Player.cs
+++ UprjKitchenChaos/Assets/Scripts/Player.cs
@@ -14,30 +14,13 @@ public class Player : MonoBehaviour
     [Tooltip("Multiplies with the transform.position to calculate the speed of the player. Normalized.")]
     [SerializeField] private float moveSpeed;
     [SerializeField] private float rotateSpeed;
+    [SerializeField] private GameInput gameInput;
     private bool isWalking;
 
     private void Update() {
-
-        //initialize input vector
-        Vector2 inputVector = new Vector2(0, 0); 
-
-        //calculate input vector with input
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = +1;
-        }
-        if (Input.GetKey(KeyCode.S)) {
-            inputVector.y = -1;
-        }
-        if (Input.GetKey(KeyCode.A)) { 
-            inputVector.x = -1;
-        }
-        if (Input.GetKey(KeyCode.D)) {
-            inputVector.x = 1;
-        }
-
-        //normalize vector
-        inputVector = inputVector.normalized;
+        //calculate movement vector
+        Vector2 inputVector = gameInput.GetMovementVectorNormalised();
+        
         //vector 2 --> vector 3
         Vector3 moveDir = new Vector3 (inputVector.x, 0, inputVector.y);
         //apply vector to position
