983425413802b1125e51c68f5bfedfc01b02f76c
professorVerbrugghe <lotrdetailsys@gmail.com>
2023-12-23 11:01:21 +0100

    Added comments to player script

--- UprjKitchenChaos/Assets/Scripts/Player.cs
+++ UprjKitchenChaos/Assets/Scripts/Player.cs
@@ -6,13 +6,15 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
     {
-
+    [Tooltip("Multiplies with the transform.position to calculate the speed of the player. Normalized.")]
     [SerializeField] private float moveSpeed;
 
     private void Update() {
 
+        //initialize input vector
         Vector2 inputVector = new Vector2(0, 0); 
 
+        //calculate input vector with input
         if (Input.GetKey(KeyCode.W))
         {
             inputVector.y = +1;
@@ -20,17 +22,18 @@ public class Player : MonoBehaviour
         if (Input.GetKey(KeyCode.S)) {
             inputVector.y = -1;
         }
-        if (Input.GetKey(KeyCode.A)) {
+        if (Input.GetKey(KeyCode.A)) { 
             inputVector.x = -1;
         }
         if (Input.GetKey(KeyCode.D)) {
             inputVector.x = 1;
         }
 
+        //normalize vector
         inputVector = inputVector.normalized;
-
+        //vector 2 --> vector 3
         Vector3 moveDir = new Vector3 (inputVector.x, 0, inputVector.y);
-
+        //apply vector to position
         transform.position += moveDir * Time.deltaTime * moveSpeed;
 
     }
